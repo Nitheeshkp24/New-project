610f789af468fa9d80ec08032ab240754dec3cb3
Athira64 <66432774+Athira64@users.noreply.github.com>
2020-10-14 13:00:18 +0530

    calculator

---
+++ calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter two numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            calc obj = new calc();
+            obj.getadd(a, b);
+        }
+    }
+}
---
+++ calculator/calc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculator
+{
+    public class calc
+    {
+        int a, b, sum;
+        public void getadd(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            sum = a + b;
+            Console.WriteLine("sum is:"+sum);  
+        }
+
+    }
+}
